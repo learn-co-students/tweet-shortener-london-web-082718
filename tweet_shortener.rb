a40760761bb24f8cc1e3e278e140d010af6bd981
Bhagya Raj <bhagyaraj01@gmail.com>
2018-08-22 18:56:43 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,5 +1,8 @@
 # Write your code here.
 
+require "pry"
+
+
 def dictionary
   convert = {
    "hello" => "hi",
@@ -31,3 +34,49 @@ end
 # 2 - indexing to the dictionary key value so that word becomes the corresponding value
 # 3 - joins up all the words  IMPLICITLY and replaced words back together to reform tweet
 
+
+# FUNCTION 2  - akes in an array of tweets, iterates over them, shortens them, and puts out the results to the screen.
+
+def bulk_tweet_shortener(tweet_array)
+  tweet_array.each do |tweet|
+    puts word_substituter(tweet)
+  end 
+end 
+
+# FUNCTION 3
+
+def selective_tweet_shortener(tweet)
+  tweet.split(" ").map do |phrase|      # 1
+    if phrase.length > 140 
+      word_substituter(phrase)
+    elsif phrase.length <= 140
+      phrase
+    end 
+  end
+end 
+  
+# FUNCTION 4 
+
+def shortened_tweet_truncator(tweet)
+  tweet.split(" ").map do |phrase|        # 1
+    if phrase.length > 140  
+      word_substituter(phrase)[0..140] + "..."
+    else
+      phrase
+    end
+  end.join(" ")
+end
+
+=begin 
+def shortened_tweet_truncator(tweet)
+  tweet.split(" ").map do |phrase|      # 1
+    if phrase.length > 140 
+      word_substituter(phrase)[0..140] + "..."
+    else 
+      phrase
+    end 
+  end.join(" ")   # 2
+end 
+=end 
+# 1 - need to convert tweet and break down into words 
+# 2 - joins up all the words  IMPLICITLY and replaced words back together to reform tweet
